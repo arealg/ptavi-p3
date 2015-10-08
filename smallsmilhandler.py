d6b2bf15e49d6c5487303bbcda1497569a31e274
Ana Real <anavm4_@hotmail.com>
2015-10-08 23:38:05 +0200

    Get_Tags

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,6 +5,8 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
+
+
 class SmallSMILHandler(ContentHandler):
 
 	def __init__(self):
@@ -19,28 +21,54 @@ class SmallSMILHandler(ContentHandler):
 
 		self.text = {'src':'','region':''}
 
-	# def get_tags(name,lista):
-	# 	etiquetas = []
-	# 	if len(etiqueta) == len(0):
-	# 		etiquetas.append(name)
-	# 	elif name not in etiquetas:
-	# 		etiqueta.append(name)
-	# 	return etiquetas + lista
+		self.etiqueta = {'root-layout': self.rl,'region':self.reg,
+				    'img': self.img,
+					'audio': self.aud,
+					'textstream': self.text}
+		self.l = []
+
+
+	def get_tags(self):
+		for i in self.etiqueta:
+			self.l.append(i)
+			self.l.append(self.etiqueta[i])
+		print(self.l)
 
 
 	def startElement(self, name, attrs):
-		etiqueta = {'root-layout': self.rl,'region':self.reg,
-		            'img': self.img,
-					'audio': self.aud,
-					'textstream': self.text}
 
-		if name in etiqueta:
-			lista = etiqueta[name]
+
+		# print(self.l)
+		# if name == 'root-layout':
+		# 	for i in self.rl:
+		# 		valor = attrs.get(i,'')
+				# self.rl[i] = valor
+			# self.l.append(self.rl)
+			# print(self.rl)
+			# self.l.append(dic)
+		# if name == 'region':
+		# 	for i in self.reg:
+		# 		valor = attrs.get(i,'')
+		# 		self.reg[i] = valor
+		# 	put_list(self.reg)
+		# if name == 'audio':
+		# 	for i in self.aud:
+		# 		valor = attrs.get(i,'')
+		# 		self.aud[i] = valor
+		# 	self.l.append(self.aud)
+		# 	print(self.l)
+
+
+
+
+			# self.l.append(dic)
+
+		if name in self.etiqueta:
+			lista = self.etiqueta[name]
 			for i in lista:
 				valor = attrs.get(i,'')
 				lista[i] = valor
 
-			print(lista)
 
 
 if __name__ == '__main__':
@@ -49,3 +77,4 @@ if __name__ == '__main__':
 	cHandler = SmallSMILHandler()
 	parser.setContentHandler(cHandler)
 	parser.parse(open('karaoke.smil'))
+	cHandler.get_tags()
