dcf3e2e2624f731c3f7b60e85125ec6268162fe2
Ana Real <anavm4_@hotmail.com>
2015-10-08 22:16:44 +0200

    Dicc

---
+++ smallsmilhandler.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+
+class SmallSMILHandler(ContentHandler):
+
+	def __init__(self):
+
+		self.rl = {'width': '','height':'','background-color': ''}
+
+		self.reg = {'id': '', 'top': '', 'bottom': '', 'left': '',
+					'right': '' }
+		self.img = {'src': '','region':'', 'begin': '','dur':''}
+		# self.aud = {'src': '', 'begin':'', 'dur': ''}
+		self.text = {'src':'','region':''}
+
+	# def get_tags(name,lista):
+	# 	etiquetas = []
+	# 	if len(etiqueta) == len(0):
+	# 		etiquetas.append(name)
+	# 	elif name not in etiquetas:
+	# 		etiqueta.append(name)
+	# 	return etiquetas + lista
+
+
+	def startElement(self, name, attrs):
+		etiqueta = {'root-layout': self.rl,'region':self.reg,
+		            'img': self.img,
+					# 'audio': self.audio,
+					'textstream': self.text}
+
+		if name in etiqueta:
+			lista = etiqueta[name]
+			for i in lista:
+				valor = attrs.get(i,'')
+				lista[i] = valor
+
+			print(lista)
+			# print(self.rl)
+			# print( self.reg)
+			# print( self.img)
+			# print( self.text)
+
+		# if name == 'region':
+		# 	for i in self.reg:
+		# 		valor = attrs.get(i,'')
+		# 		self.reg[i] = valor
+		#
+		# 	print(self.reg)
+
+
+
+if __name__ == '__main__':
+
+	parser = make_parser()
+	cHandler = SmallSMILHandler()
+	parser.setContentHandler(cHandler)
+	parser.parse(open('karaoke.smil'))
