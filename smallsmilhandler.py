87666e0739bddcdc9a99b62233791fbe0d10c699
Ana Real <anavm4_@hotmail.com>
2015-10-14 20:15:50 +0200

    Tabs

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -6,47 +6,34 @@ from xml.sax.handler import ContentHandler
 import sys
 
 
-
-
 class SmallSMILHandler(ContentHandler):
 
-	def __init__(self):
+    def __init__(self):
+        self.rl = {'width': '', 'height': '', 'background-color': ''}
+        self.reg = {'id': '', 'top': '', 'bottom': '', 'left': '',
+                    'right': ''}
+        self.img = {'src': '', 'region': '', 'begin': '', 'dur': ''}
+        self.aud = {'src': '', 'begin': '', 'dur': ''}
+        self.text = {'src': '', 'region': ''}
+        self.etiqueta = {'root-layout': self.rl, 'region': self.reg,
+                         'img': self.img, 'audio': self.aud,
+                         'textstream': self.text}
+    self.l = []
 
-		self.rl = {'width': '','height':'','background-color': ''}
-		self.reg = {'id': '', 'top': '', 'bottom': '', 'left': '',
-					'right': '' }
-		self.img = {'src': '','region':'', 'begin': '','dur':''}
-		self.aud = {'src': '', 'begin':'', 'dur': ''}
-		self.text = {'src':'','region':''}
-		self.etiqueta = {'root-layout': self.rl,'region':self.reg,
-				    'img': self.img,
-					'audio': self.aud,
-					'textstream': self.text}
-		self.l = []
+    def get_tags(self):
+        return self.l
 
+    def startElement(self, name, attrs):
+        if name in self.etiqueta:
+            lista = {i: attrs.get(i, '') for i in self.etiqueta[name]}
+            self.l.append(name)
+            self.l.append(lista)
 
-	def get_tags(self):
-		return self.l
-
-
-	def startElement(self, name, attrs):
-		if name in self.etiqueta:
-			lista = {i: attrs.get(i,'') for i in self.etiqueta[name]}
-			self.l.append(name)
-			self.l.append(lista)
-
-	# def startElement(self, name, attrs):
-	# 	if name in self.etiqueta:
-	# 		lista = {}
-	# 		for i in self.etiqueta[name]:
-	# 			lista[i]= attrs.get(i,'')
-	# 		self.l.append(name)
-	# 		self.l.append(lista)
 
 if __name__ == '__main__':
 
-	parser = make_parser()
-	cHandler = SmallSMILHandler()
-	parser.setContentHandler(cHandler)
-	parser.parse(open('karaoke.smil'))
-	cHandler.get_tags()
+    parser = make_parser()
+    cHandler = SmallSMILHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    cHandler.get_tags()
