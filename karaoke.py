2a153dd0c27d8bb408b04de4545034c5ba61afbf
Ana Real <anavm4_@hotmail.com>
2015-10-17 12:11:32 +0200

    PEP8 sin errores

--- karaoke.py
+++ karaoke.py
@@ -11,50 +11,50 @@ import json
 
 class KaraokeLocal():
 
-	def __init__(self, fichero):
-		parser = make_parser()
-		cHandler = smallsmilhandler.SmallSMILHandler()
-		parser.setContentHandler(cHandler)
-		parser.parse(open(fichero))
-		self.etiquetas = cHandler.get_tags()
+    def __init__(self, fichero):
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(open(fichero))
+        self.etiquetas = cHandler.get_tags()
 
-	def __str__(self):
-		a = ''
-		for valor in self.etiquetas:
-			if valor == str(valor):
-				etiqueta = valor
-				a += etiqueta + '\t'
-			else:
-				for i in valor.keys():
-					if valor[i] != '':
-						a += i + '=' + '"' + valor[i]+ '"' + '\t'
-				a = a + '\n'
-		a = a[:-1]
-		return a
+    def __str__(self):
+        lista_str = ''
+        for tag_attrs in self.etiquetas:
+            if tag_attrs == str(tag_attrs):
+                etiqueta = tag_attrs
+                lista_str += etiqueta + '\t'
+            else:
+                for valor in tag_attrs.keys():
+                    if tag_attrs[valor] != '':
+                        lista_str += (valor + '=' + '"'
+                                      + tag_attrs[valor] + '"' + '\t')
+                lista_str = (lista_str + '\n')
+        lista_str = lista_str[:-1]
+        return lista_str
 
-	def do_json(self, fich_smil, fich_json = ''):
-		if fich_json == '':
-			fich_json = fich_smil.split('.')[0] + '.json'
-		with open(fich_json, 'w') as file:
-			json.dump(self.etiquetas, file, sort_keys=True, indent=4)
+    def do_json(self, fich_smil, fich_json=''):
+        if fich_json == '':
+            fich_json = fich_smil.split('.')[0] + '.json'
+        with open(fich_json, 'w') as file:
+            json.dump(self.etiquetas, file, sort_keys=True, indent=4)
 
-
-	def do_local(self):
-		d = [dicc for dicc in self.etiquetas if dicc != str(dicc)]
-		for i in d:
-			if 'src' in i and 'http:' in i['src'].split('/')[0]:
-				load.urlretrieve(i['src'], i['src'].split('/')[-1])
-				i['src'] = i['src'].split('/')[-1]
+    def do_local(self):
+        lista_dicc = [dato for dato in self.etiquetas if dato != str(dato)]
+        for dicc in lista_dicc:
+            if 'src' in dicc and 'http:' in dicc['src'].split('/')[0]:
+                load.urlretrieve(dicc['src'], dicc['src'].split('/')[-1])
+                dicc['src'] = dicc['src'].split('/')[-1]
 
 
 if __name__ == '__main__':
 
-	if len(sys.argv) != 2:
-		sys.exit('Usage: python3 karaoke.py file.smil')
+    if len(sys.argv) != 2:
+        sys.exit('Usage: python3 karaoke.py file.smil')
 
-	karaoke = KaraokeLocal(sys.argv[1])
-	print(karaoke.__str__())
-	# karaoke.do_json(sys.argv[1])
-	karaoke.do_local()
-	# karaoke.do_json(sys.argv[1],'local.json')
-	print(karaoke.__str__())
+    karaoke = KaraokeLocal(sys.argv[1])
+    print(karaoke.__str__())
+    karaoke.do_json(sys.argv[1])
+    karaoke.do_local()
+    karaoke.do_json(sys.argv[1], 'local.json')
+    print(karaoke.__str__())
